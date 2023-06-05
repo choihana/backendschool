d579def542dacd453bcffbf52ed0471724f7dea0
Joey <hanazzang999@gmail.com>
2023-06-05 14:18:50 +0900

    python 기초

---
+++ Python 실습/main.py
@@ -0,0 +1,60 @@
+#변수선언
+# a = 10
+# b = 3.14
+# c = "Hello, hana"
+# d = [1,2,3]
+# e = (4,5,6)
+# f= {7,8,9}
+# g = {"apple":1, "banana" : 2 , "orange" : 3}
+
+# 데이터타입 출력
+
+# print("a: " , type(a))
+# print("b: ",type(b))
+# print("c: ",type(c))
+# print("d: ",type(d))
+# print("e: ", type(e))
+# print("f : " ,type(f))
+# print("g: ",type(g))
+
+# a= 10
+# b= 3
+#
+# print("a:",a," ,b:",b)
+# print("a+b: ", a+b) #덧셈
+# print("a/b: ",a/b) #나누기
+# print("a%b: ",a%b) #나머지
+# print("a//b: ",a//b) #몫
+# print("a**b: ",a**b) #제곱
+
+#논리 연산자
+# a = 5
+# b = 2
+# c = 3
+#
+# print ("a > b and a < c :" , a > b and a < c) # true and false = false
+# print ("a > b and c > b:", a > b and c > b) # true and true = true
+# print("a > b or a < c:" , a > b or a < c) # true or false  = true
+# print("a > b and not(a < c):" ,a > b and not(a<c)) # true and true = true
+
+# 할당 연산자
+a = 10
+b = 5
+m = 20
+
+y = a + b
+print("y = a + b: ", y)
+
+# 멤버 in 연산자
+st1 = "Welcome to 멋쟁이사자"
+print("to" in st1)
+
+j = "Hello"
+k = "like lion"
+print(type(j+k)) # str
+
+a = 5
+b = 2
+print(type(a/b)) #float
+print(type(int(a/b))) #int -> 명시적 형변환 일어남
+
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # backendschool
 - 5/30: javascript 자료형
 - 5/31: javascript 비교 연산자 , python 가상환경 구동 
+- 6/1~6/2: todo list 만들기 실습 
+- 6/5: 파이썬 기초 문법 
