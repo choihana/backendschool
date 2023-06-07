122792e509cb743c39676b12645c08088213714f
Joey <hanazzang999@gmail.com>
2023-06-07 15:04:38 +0900

    배열실습

--- Python 실습/main.py
+++ Python 실습/main.py
@@ -1,43 +1,79 @@
-#0607. if문 예제
-age = 18
-if age >= 18:
-    print("성인입니다.")
-else:
-    print("미성년자입니다.")
+# # while
+# i = 0
+# while i<10:
+#     i+=1
+#     print(i)
+# else:
+#     print("end")
+#
+# a = 2
+# while a <= 20:
+#     print(a)
+#     a += 2
+# print("코드종료")
+#
+# i=0
+# while True:
+#     i += 1
+#     print(i)
+#     if i == 5:
+#         break
 
-# 중첩된 If else (잘사용하지 않음)
-score = 85
-if score >= 90:
-    print("a")
-else:
-    if  score >= 80:
-        print("b")
-    else:
-        print("c")
-if elseif
-marks = 75
-if marks >= 90:
-    print("a")
-elif marks >= 80:
-    print("b")
-elif marks >= 75:
-    print("c")
-else:
-    print("d")
+# for문
+# for i in range(-1,-10,-2):
+#     print(i)
+#
+# r = range(5,0,-1)
+# print(r[0])
+# print(r[1])
+# print(r[2])
+# print(r[3])
+# print(r[4])
+#
 
-a = int(input("enter number greater then 2:"))
-if (a > 2):
-    print("your num:" ,a)
-else:
-    print("enter  again")
+# st = "멋쟁이사자"
+# for ch in st:
+#     print(ch)
+# else:
+#     print("end")
 
-day = input("요일을 입력하세요:")
-if day == "mon":
-    print("월요일")
-elif day == "tue":
-    print("화요일")
-elif day == "wed":
-    print("수요일")
-else:
-    print("휴일")
-습
+# pass 문
+# a = 0
+# if a < 1:
+#     pass
+# else:
+#     print("else")
+
+#배열
+import array
+stu_roll = array.array('i',[101,102,103,104,105])
+print(stu_roll[0], stu_roll[4])
+
+for element in stu_roll:
+    print(element, end=' ' )
+
+stu_roll.insert(1, 106)
+stu_roll.insert(3,107)
+print('\n')
+print(stu_roll)
+i =0
+n = len(stu_roll)
+while i < n:
+    print(stu_roll[i], end = '')
+    i+=1
+
+stu_roll.remove(107)
+print(stu_roll, len(stu_roll))
+
+stu_roll.pop()
+print(stu_roll, len(stu_roll))
+
+print(stu_roll,stu_roll.pop(2))
+print(stu_roll.index(104))
+#extend
+stu_roll2 = array.array('i',[201,202,203])
+stu_roll.extend(stu_roll2)
+print(stu_roll)
+#reverse
+stu_roll.reverse()
+print(stu_roll)
