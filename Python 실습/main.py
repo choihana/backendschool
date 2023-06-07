cadc2e12d69c3f053fde64e8e375e78475535960
Joey <hanazzang999@gmail.com>
2023-06-07 09:33:14 +0900

    if문 실습

--- Python 실습/main.py
+++ Python 실습/main.py
@@ -1,60 +1,43 @@
-#변수선언
-# a = 10
-# b = 3.14
-# c = "Hello, hana"
-# d = [1,2,3]
-# e = (4,5,6)
-# f= {7,8,9}
-# g = {"apple":1, "banana" : 2 , "orange" : 3}
+#0607. if문 예제
+age = 18
+if age >= 18:
+    print("성인입니다.")
+else:
+    print("미성년자입니다.")
 
-# 데이터타입 출력
+# 중첩된 If else (잘사용하지 않음)
+score = 85
+if score >= 90:
+    print("a")
+else:
+    if  score >= 80:
+        print("b")
+    else:
+        print("c")
+if elseif
+marks = 75
+if marks >= 90:
+    print("a")
+elif marks >= 80:
+    print("b")
+elif marks >= 75:
+    print("c")
+else:
+    print("d")
 
-# print("a: " , type(a))
-# print("b: ",type(b))
-# print("c: ",type(c))
-# print("d: ",type(d))
-# print("e: ", type(e))
-# print("f : " ,type(f))
-# print("g: ",type(g))
+a = int(input("enter number greater then 2:"))
+if (a > 2):
+    print("your num:" ,a)
+else:
+    print("enter  again")
 
-# a= 10
-# b= 3
-#
-# print("a:",a," ,b:",b)
-# print("a+b: ", a+b) #덧셈
-# print("a/b: ",a/b) #나누기
-# print("a%b: ",a%b) #나머지
-# print("a//b: ",a//b) #몫
-# print("a**b: ",a**b) #제곱
-
-#논리 연산자
-# a = 5
-# b = 2
-# c = 3
-#
-# print ("a > b and a < c :" , a > b and a < c) # true and false = false
-# print ("a > b and c > b:", a > b and c > b) # true and true = true
-# print("a > b or a < c:" , a > b or a < c) # true or false  = true
-# print("a > b and not(a < c):" ,a > b and not(a<c)) # true and true = true
-
-# 할당 연산자
-a = 10
-b = 5
-m = 20
-
-y = a + b
-print("y = a + b: ", y)
-
-# 멤버 in 연산자
-st1 = "Welcome to 멋쟁이사자"
-print("to" in st1)
-
-j = "Hello"
-k = "like lion"
-print(type(j+k)) # str
-
-a = 5
-b = 2
-print(type(a/b)) #float
-print(type(int(a/b))) #int -> 명시적 형변환 일어남
-
+day = input("요일을 입력하세요:")
+if day == "mon":
+    print("월요일")
+elif day == "tue":
+    print("화요일")
+elif day == "wed":
+    print("수요일")
+else:
+    print("휴일")
+습
