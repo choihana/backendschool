907e1261b4e54ea1595fb300b840eb8c802b648c
Joey <hanazzang999@gmail.com>
2023-06-08 11:12:05 +0900

    리스트 실습 1

--- Python 실습/main.py
+++ Python 실습/main.py
@@ -1,30 +1,11 @@
-def pw(x, y):
-    z = x ** y
-    print(z)
-
-
-pw(2, 5)
-
-
-def show(name, age):
-    print(f"Name: {name}  Age : {age}")
-
-
-show("hana", 31)
-show(age=29, name="hana2")  # keyword적을경우 순서 변경 가능
-
-
-def add(*num):
-    z = num[0] + num[1] + num[2]
-    print(z)
-
-
-add(5, 2, 4)
-
-
-def add2(x, *num):
-    print(x, num[0], num[2], num[1])
-
-
-add2(1, 10, 20, 30)
-
+fruits = ["apple","banana","cherry","orange"]
+print("첫 리스트:",fruits)
+fruits.append("grape")
+print("append: ",fruits)
+fruits.insert(2, "kiwi")
+print("insert kiwi: ",fruits)
+print("pop: ",fruits.pop(), fruits)
+fruits.insert(1,"cherry")
+print("insert cherry: ",fruits)
+fruits.remove("cherry")
+print("remove cherry: ",fruits) #첫번째만 지움
