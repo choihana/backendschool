31d8ab6b714379ef61571bec2d4cfaa09dfd821c
Joey <hanazzang999@gmail.com>
2023-06-08 15:44:45 +0900

    lambda 함수

--- Python 실습/main.py
+++ Python 실습/main.py
@@ -1,19 +1,4 @@
-a = {10, 20, 30 }
-print("초기 세팅",a )
-a.add(15)
-print("add: ",a)
-a.update([10,15,20,25,35])
-print("update:" , a)
-a.remove(35)
-print("remove:", a)
-a.discard(10)
-print("discard:", a)
-b = {20,25,30,35}
-a_b = a.intersection(b)
-print("b:",b)
-print("intersection:",a_b)
-print("union:",a.union((b)))
-print("diff a-b:",a.difference(b))
-print("diff b-a:",b.difference(a))
-print(b.discard(5))
-print(a&b)
+add = lambda x,y: (x+y)
+print(add(1,2))
+add2 = lambda x, y=4: x+y
+print(add2(1))
