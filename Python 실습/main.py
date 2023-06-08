c80c6deed75ce7f88068ae392c8a0926f425a29f
Joey <hanazzang999@gmail.com>
2023-06-08 14:49:06 +0900

    set 실습

--- Python 실습/main.py
+++ Python 실습/main.py
@@ -1,12 +1,19 @@
-def generate_alphabet(start_letter, end_letter):
-    start = ord(start_letter)
-    end = ord(end_letter)
-    while start <= end:
-        yield chr(start)
-        start +=1
-
-runner = generate_alphabet('A','F')
-
-for letter in runner:
-    print(letter)
-
+a = {10, 20, 30 }
+print("초기 세팅",a )
+a.add(15)
+print("add: ",a)
+a.update([10,15,20,25,35])
+print("update:" , a)
+a.remove(35)
+print("remove:", a)
+a.discard(10)
+print("discard:", a)
+b = {20,25,30,35}
+a_b = a.intersection(b)
+print("b:",b)
+print("intersection:",a_b)
+print("union:",a.union((b)))
+print("diff a-b:",a.difference(b))
+print("diff b-a:",b.difference(a))
+print(b.discard(5))
+print(a&b)
