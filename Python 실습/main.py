e05d93a0ee9fcb37aae2f42772a16169a1f7fa67
Joey <hanazzang999@gmail.com>
2023-06-09 10:04:26 +0900

    class 실습 1

--- Python 실습/main.py
+++ Python 실습/main.py
@@ -1,4 +1,24 @@
-add = lambda x,y: (x+y)
-print(add(1,2))
-add2 = lambda x, y=4: x+y
-print(add2(1))
+class Mobile:
+    fp = "yes"
+
+realme = Mobile()
+redme = Mobile()
+geek = Mobile()
+
+print("1",Mobile.fp)
+print("1",realme.fp)
+print("1",redme.fp)
+print("1",geek.fp)
+
+Mobile.fp = "no"
+print("2",Mobile.fp)
+print("2",realme.fp)
+print("2",redme.fp)
+print("2",geek.fp)
+
+realme.fp = "Not Working"
+print("3",Mobile.fp)
+print("3",realme.fp)
+print("3",redme.fp)
+print("3",geek.fp)
+
