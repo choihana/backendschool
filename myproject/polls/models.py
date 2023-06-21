1de498890c9a558e009d84c7a65a6f9f5f52e8b1
Joey <hanazzang999@gmail.com>
2023-06-21 14:11:12 +0900

    view 추가

--- myproject/polls/models.py
+++ myproject/polls/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model): #질문
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model): #답변
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    #question 삭제되면 같이 삭제됨
+    choice_text=models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
