aff6539652c7ddce9d9eafe4cfc1af5b9fd85698
Joey <hanazzang999@gmail.com>
2023-06-26 16:11:15 +0900

    blog 기본2

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, ArchiveIndexView, YearArchiveView, MonthArchiveView, \
+    DayArchiveView, TodayArchiveView
 
 from blog.models import Post
 
@@ -11,5 +12,36 @@ class PostLV(ListView):
     context_object_name = 'posts'
     paginate_by = 2
 
+
 class PostDV(DetailView):
-    model=Post
+    model = Post
+
+
+class PostAV(ArchiveIndexView):
+    model = Post
+    date_field = 'modify_dt'
+
+
+class PostYAV(YearArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+    make_object_list = True
+    # month_format = '%b'
+
+
+class PostMAV(MonthArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+    month_format = '%m'
+
+
+class PostDAV(DayArchiveView):
+    model = Post
+    month_format = '%m'
+    date_field = 'modify_dt'
+
+
+class PostTAV(TodayArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+    template_name = 'blog/post_archive_day.html'
