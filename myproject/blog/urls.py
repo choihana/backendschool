3adcd6f5c288822a70d547b4601a7008aae3a39b
Joey <hanazzang999@gmail.com>
2023-06-26 15:10:21 +0900

    blog 기본

--- myproject/blog/urls.py
+++ myproject/blog/urls.py
@@ -4,6 +4,7 @@ from blog import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.PostLV.as_view(), name='index'),
+    path('post/', views.PostLV.as_view(), name='post_list'),
     re_path(r'^post/(?P<slug>[-\w]+)/$', views.PostDV.as_view(), name='post_detail'),
 
 ]
